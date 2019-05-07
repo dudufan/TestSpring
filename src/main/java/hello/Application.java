ed0e1bca10939c477f32674dcf81e741eb38fcaa
dudufan <dudufan108@qq.com>
2019-05-08 07:14:07 +0800

    add shiro

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,16 +3,10 @@ package hello;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -21,13 +15,18 @@ public class Application {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws InterruptedException {
-        testRedisMessage();
+        ApplicationContext ctx = SpringApplication.run(Application.class);
+        ((ConfigurableApplicationContext) ctx).registerShutdownHook();
+//        testRedisMessage(ctx);
+        Book book = ctx.getBean(Book.class);
+        LOGGER.info("book.name:"+book.name);
+        LOGGER.info("book.author:"+book.author);
 
         System.exit(0);
     }
 
-    private static void testRedisMessage() throws InterruptedException {
-        ApplicationContext ctx = SpringApplication.run(Application.class);
+    private static void testRedisMessage(ApplicationContext ctx) throws InterruptedException {
+
         StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
         CountDownLatch latch = ctx.getBean(CountDownLatch.class);
         LOGGER.info("Sending message...");
